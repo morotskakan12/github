67f5f926d83469dba81024cf134934015387d1e4
isak <isak.bl@hotmail.com>
2019-03-31 18:19:03 +0200

    2019-03-31
    la till komenater och mellan rumm

--- hängaGubben/StartPege.java
+++ hängaGubben/StartPege.java
@@ -12,41 +12,51 @@ public class StartPege{
 		boolean innerLopp= true;
 		// två funktoner om man vill starra om spelet och om men skriver in fel ska pogramet inte stängas ner 
 		System.out.println("hej och välkomen.");
+		// initcierar pogram lopp som bryt när lopp en endras till fals 
 		while(lopp == true){
 		innerLopp= true;
 		System.out.println("vill du spela på Svenska eller Engelska");
 		System.out.println("hello and welcome. you want to play on Swedish or English");
 		String hiddenWord = sc.nextLine();
+		System.out.println("");
+		// val av händelse 
 		if ((hiddenWord.equals("Svenska"))||(hiddenWord.equals("Swedish"))){
+			// mitoden för att köra pogramet igen eller inte
 			while(innerLopp==true){
 			System.out.println("vill du spela igen");
 			System.out.println("svara Ja eller Nej");
+			System.out.println("");
 			String playagen = sc.nextLine();
 			if (playagen.equals("Nej")){
 				lopp = false;
 				innerLopp= false;
 			}else if(playagen.equals("Ja")){
-				break; 
+				innerLopp= false;
 			}else{
+			// ifal anvenaren skriver fel
 			System.out.println("fel gör om");
-			
+			System.out.println("");
 			}
 			}
 		}else if ((hiddenWord.equals("Engelska"))||(hiddenWord.equals("English"))){
+			// mitoden för att köra pogramet igen eller inte
 			while(innerLopp==true){
 				System.out.println("you want to play again");
 				System.out.println("answer Yes or No");
+				System.out.println("");
 				String playagen = sc.nextLine();
 				if (playagen.equals("No")){
 					lopp = false;
 					innerLopp= false;
 				}else if(playagen.equals("Yes")){
-					break;
+					innerLopp= false;
+				// ifal anvenaren skriver fel
 				}else{
 				System.out.println("try again");
 				System.out.println("");
 				}
 			}
+		// ifal anvenaren skriver fel
 		}else{
 			System.out.println("Du kan bara skriva in Svenska eller Engelska. vänligen försök igen");
 			System.out.println("You can only enter Swedish or English. please try again");
@@ -54,6 +64,7 @@ public class StartPege{
 		}
 		
 	}
+	//avslut
 		System.out.println("");
 		System.out.println("basi basi");
 }
